5a354516a37511f7f954c8c154df980289d22ede
Renan Dantas <111248556+RenanDaantas@users.noreply.github.com>
2022-08-30 11:41:08 -0300

    002 - DATA DE NASCIMENTO
    Nesse segundo programa, explorei a coleta de informações baseado na data de nascimento do usuário, solicitando que informasse o dia, mês e ano de nascimento, ao final retornaria um print, utilizando as informações concedidas.

---
+++ 002.py
@@ -0,0 +1,5 @@
+dia = input('Qual o dia do seu aniversário?')
+mes = input('Qual mês você nasceu?')
+ano = input('Qual ano de nascimento?')
+print('Você nasceu no dia', dia, 'de', mes,'de', ano, '. Correto?')
+      
