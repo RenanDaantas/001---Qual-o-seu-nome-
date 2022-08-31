c00de09f8548d59deaa936db098122436efe0c1c
Renan Dantas <111248556+RenanDaantas@users.noreply.github.com>
2022-08-31 11:47:40 -0300

    007 - MÉDIA DO ALUNO
    Nesse caso é um desenvolvimento padrão onde todos realizam para aprender de forma simples e rápida pra trabalhar recebendo dois ou mais valores e fazendo sua aplicação nos cálculos, ajuda também no entendimento da ordem de procedência do operadores aritméticos. Um exercício simples, onde basta receber dois valores | float | no caso as duas notas do aluno e calcular sua média, somando os dois valores e dividindo por 2, mostrando ao final no | print | a média do aluno.

---
+++ 007.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de uma aluno, calcule e mostre a sua média
+n1 = float(input('Digite sua primeira nota aqui:'))
+n2 = float(input('Digite sua segunda nota aqui:'))
+m = (n1+n2)/2
+print('Sua média é {}'.format(m))
